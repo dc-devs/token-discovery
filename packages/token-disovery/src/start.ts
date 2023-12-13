33b6a5ca07928a55d5b9a80a6905788179bc1ea7
David Christian <david.w.christian@gmail.com>
2023-12-13 07:10:53 -0800

    Added token tracker to token discovery

--- packages/token-disovery/src/start.ts
+++ packages/token-disovery/src/start.ts
@@ -1,3 +1,20 @@
-// import { TokenPairTracker } from '@token-discovery/token-tracker';
+import { enums, utilClasses } from '@token-discovery/common';
+import { TokenPairTracker } from '@token-discovery/token-tracker';
 
-// TokenPairTracker.start()
+const pubSub = new utilClasses.PubSub();
+const tokenPairTracker = new TokenPairTracker({ pubSub });
+
+tokenPairTracker.start();
+
+tokenPairTracker.pubSub.on(
+	enums.PubSubEvent.NewTokenPairCreated,
+	({ token0, token1, reserve0, reserve1, pairAddress }) => {
+		tokenPairTracker.logTokenPair({
+			token0,
+			token1,
+			reserve0,
+			reserve1,
+			pairAddress,
+		});
+	},
+);
